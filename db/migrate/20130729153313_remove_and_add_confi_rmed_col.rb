fcb6d1b32cd806948ea07436d08b54c682286aa9
James <jlhaley1@gmail.com>
2013-08-06 16:57:04 -0700

    removed file

--- db/migrate/20130729153313_remove_and_add_confi_rmed_col.rb
+++ db/migrate/20130729153313_remove_and_add_confi_rmed_col.rb
@@ -1,6 +1,5 @@
 class RemoveAndAddConfiRmedCol < ActiveRecord::Migration
   def change
-    remove_column :waves, :confimed
     add_column :waves, :confirmed, :boolean, :default => false
   end
 end
